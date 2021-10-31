0cd3a71efc1650e810fd91766460bf2cbdea861f
Mattéo Legagneux <matteoo34@icloud.com>
2021-10-31 10:59:13 +0100

    Mobile touch

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,16 +14,29 @@ P = 2, // Smooth motion (higher number = smoother motion)
 // Mouse events
 M = {
 	down: function(e) {
-		R.on.x = -w2 + e.clientX;
+		if (!e.clientX) {
+			// Touch screen
+			R.on.x = -w2 + e.touches[0].clientX;
+			R.on.y = h2 + -e.touches[0].clientY
+		} else {
+			R.on.x = -w2 + e.clientX;
+			R.on.y = h2 + -e.clientY
+		}
 		R.old.x = R.x;
-		R.on.y = h2 + -e.clientY;
 		R.old.y = R.y;
-		document.addEventListener("mousemove", M.move)
+		document.addEventListener("mousemove", M.move);
+		document.addEventListener("touchmove", M.move)
 	},
 	move: function(e) {
-		R.now.x = -w2 + e.clientX;
+		if (!e.clientX) {
+			// Touch screen
+			R.now.x = -w2 + e.touches[0].clientX;
+			R.now.y = h2 + -e.touches[0].clientY;
+		} else {
+			R.now.x = -w2 + e.clientX;
+			R.now.y = h2 + -e.clientY;
+		}
 		R.x = ((R.now.x - R.on.x) / S) + R.old.x;
-		R.now.y = h2 + -e.clientY;
 		R.y = ((R.now.y - R.on.y) / S) + R.old.y;
 		if (R.x < -360) R.x += 360;
 		if (R.x > 360) R.x -= 360;
@@ -32,19 +45,23 @@ M = {
 		// Model motion
 		let transform = `rotateX(${R.y.toFixed(P)}deg) rotateZ(${-R.x.toFixed(P)}deg)`;
 		model.style["-webkit-transform"] = transform;
-		model.style["-ms-transform"] = transform;
 		model.style.transform = transform
 	},
-	up: function() {document.removeEventListener("mousemove", M.move)}
+	up: function() {
+		document.removeEventListener("mousemove", M.move);
+		document.removeEventListener("touchmove", M.move)
+	}
 },
 // Model & faces selectors
 model = document.querySelector("#model");
 let facesDisplayed = false;
+
 // Model initial rotation angle
 let transform = `rotateX(${R.y.toFixed(P)}deg) rotateY(${R.x.toFixed(P)}deg)`;
 model.style["-webkit-transform"] = transform;
-model.style["-ms-transform"] = transform;
 model.style.transform = transform;
 // Event listeners
 addEventListener("mousedown", M.down);
-addEventListener("mouseup", M.up)
+addEventListener("touchstart", M.down);
+addEventListener("mouseup", M.up);
+addEventListener("touchend", M.up)
